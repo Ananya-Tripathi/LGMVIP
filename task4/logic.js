af90e1352c2a61d060fd9880dd2b7e185b47d788
ANANYA TRIPATHI <85894546+Ananya-Tripathi@users.noreply.github.com>
2022-08-25 22:07:55 +0530

    Add files via upload

---
+++ task4/logic.js
@@ -0,0 +1,27 @@
+var screen=document.querySelector('#screen');
+var btn=document.querySelectorAll('.btn');
+
+/*============ For getting the value of btn, Here we use for loop ============*/
+for(item of btn)
+{
+    item.addEventListener('click',(e)=>{
+        btntext=e.target.innerText;
+
+        if(btntext =='×')
+        {
+            btntext= '*';
+        }
+
+        if(btntext=='÷')
+        {
+            btntext='/';
+        }
+        screen.value+=btntext;
+    });
+}
+
+
+function backspc()
+{
+    screen.value=screen.value.substr(0,screen.value.length-1);
+}
